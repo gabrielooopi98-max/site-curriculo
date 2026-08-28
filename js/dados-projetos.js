b45f88cafe2c5a6f95e44fe41110edc37d67d7ac
gabrielooopi98-max <gabrielooopi98@gmail.com>
2026-08-28 10:46:26 -0300

    primeiro commit

---
+++ js/dados-projetos.js
@@ -0,0 +1,45 @@
+const projetos = [
+    {
+        titulo: "Biblioteca de Jogos",
+        descricao: "Plataforma de busca de jogos desenvolvida em React, com filtro em tempo real que atualiza os resultados instantaneamente. Focado em componentização, gerenciamento de estado com hooks e otimização de performance.",
+        imagem: "imagens/img-projeto-biblioteca-de-jogos.png",
+        linguagens: [
+            { nome: "JavaScript", porcentagem: 60.7, cor: "#f1c40f" },
+            { nome: "CSS", porcentagem: 38.5, cor: "#6c3ce9" },
+            { nome: "HTML", porcentagem: 0.8, cor: "#e8491d" }
+        ],
+
+        links: {
+            github: "https://github.com/gabrielooopi98-max/Biblioteca-de-jogos",
+            verSite: "https://gabrielooopi98-max.github.io/Biblioteca-de-jogos/"
+        }
+    },
+    {
+        titulo: "Confeitaria de Bolos",
+        descricao: "Landing page para confeitaria artesanal com cardápio interativo, fluxo de encomendas e integração com WhatsApp. Projeto centrado em conversão e experiência do usuário para pequenos negócios.",
+        imagem: "imagens/img-projeto-confetaria.png",
+        linguagens: [
+            { nome: "JavaScript", porcentagem: 28.5, cor: "#f1c40f" },
+            { nome: "CSS", porcentagem: 43.3, cor: "#6c3ce9" },
+            { nome: "HTML", porcentagem: 28.2, cor: "#e8491d" }
+        ],
+        links: {
+            github: "https://github.com/gabrielooopi98-max/confetaria",
+            verSite: "https://gabrielooopi98-max.github.io/confetaria/"
+        }
+    },
+    {
+        titulo: "App Financeiro",
+        descricao: "Aplicação completa de controle financeiro pessoal com dashboard, gráficos de gastos, categorias personalizáveis e relatórios detalhados. Desenvolvido com React e integração com local storage.",
+        imagem: "imagens/img-projeto-financeiro.png",
+        linguagens: [
+            { nome: "JavaScript", porcentagem: 33.8, cor: "#f1c40f" },
+            { nome: "CSS", porcentagem: 44.1, cor: "#6c3ce9" },
+            { nome: "HTML", porcentagem: 22.1, cor: "#e8491d" }
+        ],
+        links: {
+            github: "https://gabrielmoreira89.github.io/Igreja-Financeiro/",
+            verSite: "https://gabrielmoreira89.github.io/Igreja-Financeiro/"
+        }
+    },
+]
